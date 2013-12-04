571e1142cb231dfa22d35a8da02900a53c823962
MakerSquare Student <students@themakersquare.com>
2013-12-04 15:00:57 -0600

    Complete seed and modify index file to display data - still working on happiness, though

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,22 +12,22 @@ places = {
 	"cota" =>{
 		name: 'Circuit of the Americas',
 		picture_url: 'http://cdn.bleacherreport.net/images_root/slides/photos/002/491/383/539px-Austin_Formula_One_circuit_display_image.jpg',
-		review: "World class racing facility in Austin"
+		review: "World class racing facility in Austin. Also has an excellent concert amphetheater."
 	},
-	"name" =>{
-		name: '',
-		picture_url: '',
-		review: ""
+	"franklin" =>{
+		name: 'Franklin BBQ',
+		picture_url: 'http://www.bonappetit.com/wp-content/uploads/2011/06/franklin-bbq-brisket.jpg',
+		review: "BBQ worth waiting in line for"
 	},
-	"name" =>{
-		name: '',
-		picture_url: '',
-		review: ""
+	"zilker" =>{
+		name: 'Zilker Park',
+		picture_url: 'http://www.travisheightsaustintx.com/wp-content/uploads/2011/05/zilker-park-off-leash-area-austin.jpg',
+		review: "Outstanding park near downtown Austin. Host of ACL Fest, the trail of lights and many other excellent happenings."
 	},
-	"name" =>{
-		name: '',
-		picture_url: '',
-		review: ""
+	"circle" =>{
+		name: 'Circle Brewing',
+		picture_url: 'http://craftprideaustin.com/wp-content/uploads/2013/04/circle-brewery.png',
+		review: "Austin is home to many excellent breweries, including Circle. Circle is in the middle of an industrial park and hosts open houses regularly. Their beer is very good"
 	},
 }
 

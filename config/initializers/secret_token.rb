d8a252237cfd1f89933f524552c1fd69d450444b
MakerSquare Student <students@themakersquare.com>
2013-12-04 13:53:06 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threesquare::Application.config.secret_key_base = 'f056b3698393ebe0e492b705cb35cc5ffb3825144875d1232a7a6ae8c60044f33410f523623cbbeeb015b9d56bca90ad5201d683dcbb4c05e1029c4423d36966'
